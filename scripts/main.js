7b74b78ca97d788dae4fbec4f83752b2bdc8e612
aprilkrg <aprilkrgonzales@gmail.com>
2023-08-08 20:19:37 -0600

    get acnh villager data as json

--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,34 @@
 console.log("good morning developers")
 
-// import axios from "axios"
-// const axios = require("axios").default
-
-// const options = {
-//     method: "GET",
-//     url: "https://animal-crossing-new-horizons2.p.rapidapi.com/api/v1/accessories",
-//     params: {
-//         page_size: "50",
-//         page: "1"
-//     },
-//     headers: {
-//         "X-RapidAPI-Key": "d659c807-1aa9-4c06-b204-1faf7ccfc4da",
-//         "X-RapidAPI-Host": "animal-crossing-new-horizons2.p.rapidapi.com"
-//     }
-// };
-
-//     // try {
-//     //     const response = await axios.request(options);
-//     //     console.log(response.data);
-//     // } catch (error) {
-//     //     console.error(error);
-//     // }
-
-// axios.request(options)
-//     .then(function(response) {
-//     // handle success
-//     console.log("DATA\n",response.data)
-//     })
-//     .catch(function(error) {
-//     // handle error
-//     console.log("ERROR=>", error)
-//     })
-//     .finally(function() {
-//     // always executed
-//     console.log("Finished Axios GET request")
-//     })
-
-
-
-const data = null;
-
+// const data = null;
 const xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
+
+// const getVillagers = function(){
+//     let villagerData = null
+//     xhr.addEventListener('readystatechange', function () {
+//         if (this.readyState === this.DONE) {
+//             console.log(this.responseText)
+//             villagerData = this.responseText
+//         }
+//     });
+//     xhr.open("GET", "https://api.nookipedia.com/villagers?api_key=d659c807-1aa9-4c06-b204-1faf7ccfc4da", true)
+//     xhr.send()
+// }
+// const result = getVillagers()
+// console.log("RESULT:", result)
 
 xhr.addEventListener('readystatechange', function () {
     if (this.readyState === this.DONE) {
-        console.log(this.responseText);
+        console.log(this.response)
     }
 });
 
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       document.getElementById("demo").innerHTML =
-//       this.responseText;
-//     }
-//   };
+xhr.open('GET', 'https://api.nookipedia.com/villagers?api_key=d659c807-1aa9-4c06-b204-1faf7ccfc4da');
 
-// xhr.open('GET', 'https://api.nookipedia.com/villagers?api_key=d659c807-1aa9-4c06-b204-1faf7ccfc4da');
-// xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
+xhr.responseType = 'json';
 
-
-// xhr.send(data);
-
-xhr.open("GET", "https://api.nookipedia.com/villagers?api_key=d659c807-1aa9-4c06-b204-1faf7ccfc4da", true);
+xhr.onload = function() {
+    let responseObj = xhr.response;
+    // console.log("RESPONSE", responseObj.message); 
+};
 xhr.send();
